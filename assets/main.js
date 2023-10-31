8181b0d680e2346d84f910b623c201e757925247
zmeu` <tsraduu@gmail.com>
2023-10-31 19:24:46 +0000

    Dark mode updated for the entire calculator

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,27 @@
 const darkMode = document.querySelectorAll("[data-bs-theme]");
 const darkModeBtn = document.getElementById("toggler");
+const buttons = document.querySelectorAll("button");
+const resultWrapper = document.querySelector(".result-wrapper");
 
 //> Functions
 function toggleDarkMode() {
   darkMode.forEach((button) => {
     if (button.dataset.bsTheme === "dark") {
+      resultWrapper.classList.remove("text-dark", "bg-light");
+      resultWrapper.classList.add("text-light", "bg-dark");
+      buttons.forEach((button) => {
+        button.classList.remove("text-dark", "bg-light");
+        button.classList.add("text-light", "bg-dark");
+      });
       button.dataset.bsTheme = "light";
     } else {
       button.dataset.bsTheme = "dark";
+      resultWrapper.classList.remove("text-light", "bg-dark");
+      resultWrapper.classList.add("text-dark", "bg-light");
+      buttons.forEach((button) => {
+        button.classList.remove("text-light", "bg-dark");
+        button.classList.add("text-dark", "bg-light");
+      });
     }
   });
 }
