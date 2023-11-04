6974faf739c4b8ba7e4916b91045d60e91134169
zmeu` <tsraduu@gmail.com>
2023-11-04 17:44:37 +0000

    Variables added

--- assets/calculator-redo.js
+++ assets/calculator-redo.js
@@ -1,3 +1,11 @@
 const previousOperandTextElement = document.querySelector(
   "[data-previous-operant]"
 );
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operant]"
+);
+const clearButton = document.querySelector("[data-clear]");
+const delButton = document.querySelector("[data-del]");
+const equalButton = document.querySelector("[data-equal]");
+const mathButtons = document.querySelectorAll("[data-action]");
+const numberButtons = document.querySelectorAll("[data-number]");
