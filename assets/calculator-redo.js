85fb06c4ffeace9c5eff9f1d85adbfcf53c382dd
zmeu` <tsraduu@gmail.com>
2023-11-05 08:41:35 +0000

    My take at creating the calculator, following GPT's written instruction at what each funciton does.
    It does not work, obviously, and I will try to compare the code and see how it goes.
    
    getDisplayNumber(number) function, i jsut copied from the other working code as I wouldn't get it right now

--- assets/calculator-redo.js
+++ assets/calculator-redo.js
@@ -14,5 +14,120 @@ class Calculator {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.currentOperandTextElement = currentOperandTextElement;
     this.previousOperandTextElement = previousOperandTextElement;
+    this.clear();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+  appendNumbers(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = number;
+  }
+
+  chooseOperation(operation) {
+    if ((this.currentOperand = "")) return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+
+  compute() {
+    let operation;
+    const prev = parseFloat(this.previousOperand);
+    const curr = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(curr)) return;
+    switch (this.operation) {
+      case "+":
+        operation = prev + curr;
+        break;
+      case "-":
+        operation = prev - curr;
+        break;
+      case "*":
+        operation = prev * curr;
+        break;
+      case "÷":
+        operation = prev / curr;
+        break;
+      default:
+        break;
+    }
+    this.currentOperand = operation;
+    operation = undefined;
+    this.currentOperand = "";
+  }
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+
+    if (isNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("en", {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
+  updateDisplay() {
+    this.currentOperandTextElement.innerHTML = getDisplayNumber(
+      this.currentOperand
+    );
+    if (this.operation !== "") {
+      this.previousOperandTextElement.innerHTML = getDisplayNumber(
+        `${this.currentOperand} ${this.operation}`
+      );
+    } else {
+      this.previousOperandTextElement = "";
+    }
   }
 }
+
+const calculator = new Calculator(
+  currentOperandTextElement,
+  previousOperandTextElement
+);
+
+clearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+delButton.addEventListener("click", (button) => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerHTML);
+    calculator.updateDisplay();
+  });
+});
+
+equalButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
+
+mathButtons.forEach((button) => {
+  button.addEventListener("click", (button) => {
+    calculator.chooseOperation();
+    calculator.updateDisplay();
+  });
+});
