2e824fcb4b012c0231d39c873472ab4e24d4b67f
zmeu` <tsraduu@gmail.com>
2023-11-09 18:52:35 +0000

    We can append a single number to the screen

--- assets/calculator-redo.js
+++ assets/calculator-redo.js
@@ -4,17 +4,25 @@ class Calculator {
     this.currentOperandElement = currentOperandElement;
   }
 
-  clear() {}
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
 
   delete() {}
 
-  appendNumbers(number) {}
+  appendNumbers(number) {
+    this.currentOperand = number;
+  }
 
   chooseOperation(operation) {}
 
   calclate() {}
 
-  updateDisplay() {}
+  updateDisplay() {
+    this.currentOperandElement.textContent = this.currentOperand;
+  }
 }
 
 const previousOperandElement = document.querySelector(
@@ -26,3 +34,20 @@ const delButton = document.querySelector("[data-del]");
 const equationButtons = document.querySelectorAll("[data-action]");
 const numberButtons = document.querySelectorAll("[data-number]");
 const equalButton = document.querySelector("[data-equal]");
+
+const calculator = new Calculator(
+  previousOperandElement,
+  currentOperandElement
+);
+
+clearButton.addEventListener("click", (button) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumbers(button.textContent);
+    calculator.updateDisplay();
+  });
+});
